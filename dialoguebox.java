d86769a547b3b91b15367262a968bbcd97eb12f0
lautanskr <lawtontharu@gmail.com>
2021-02-22 16:13:12 +0545

    complete box

--- dialoguebox.java
+++ dialoguebox.java
@@ -12,5 +12,21 @@ DialogDemo(){
     setDefaultLookAndFeelDecorated(true);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
+    setTitle("paraent Frame");
+    I = new JLabel("Email");
+    f = new JTextField(15);
+    d = new JDialog(this, "Dialog Example", true);
+    d.setLayout(new FlowLayout(FlowLayout.CENTER,20,20));
+    d.setSize(300,200);
+    d.setLocation(50,50);
+    d.setModal(true);
+    d.add(l);
+    d.add(f);
+    d.setVisible(true);
+
+}
+public static void main(String args[])
+{
+    DialogDemo frame = new DialogDemo();
 }
 }
