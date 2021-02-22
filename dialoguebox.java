5bfed3642cc96bb8747aa545ecbf94cb9307184e
lautanskr <lawtontharu@gmail.com>
2021-02-22 16:05:31 +0545

    email dialogue box

---
+++ dialoguebox.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class DialogDemo extends JFrame{
+
+JLabel l;
+JTextField f;
+JDialog d;
+DialogDemo(){
+
+    setSize(400,300);
+    setDefaultLookAndFeelDecorated(true);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+}
+}
